f68742ff8fa46640cd5c9ecfa958993d67d4bd0f
Agata <modjeskaa@gmail.com>
2023-08-21 16:18:02 +0200

    Add new files

---
+++ create_folders.py
@@ -0,0 +1,27 @@
+import os
+
+base_path = r'C:\Users\AgataPC\Desktop\projekciki\DATA_SCIENCE'
+
+#                   CREATE X NUMBER OF FOLDERS
+# num_folders = 4
+# folder_list = [f"folder_{i}" for i in range(1, num_folders + 1)]
+
+# for folder_name in folder_list:
+#     folder_path = os.path.join(base_path, folder_name)
+#     os.mkdir(folder_path)
+
+
+#                   CREATE FOLDERS WITH NAMES
+# folders_names = [
+#     "1 Regression",
+#     "2 Classification", 
+#     "3 Clustering",
+#     "4 Deep Learning"
+# ]
+
+# for folder_name in folders_names:
+#     folder_path = os.path.join(base_path, folder_name)
+#     os.makedirs(folder_path, exist_ok=True)
+
+
+
